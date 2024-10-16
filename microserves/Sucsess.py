c0d2bf64e2d1502c803b089c1f4661f4a235034c
Perforatorr <113392885+Perforatorr@users.noreply.github.com>
2024-10-16 14:21:48 +0300

    Create Sucsess.py

---
+++ microserves/Sucsess.py
@@ -0,0 +1,9 @@
+from flask import jsonify
+def success701():
+    return jsonify({"success": "The record has been added"}), 701
+
+def success702():
+    return jsonify({"success": "The record has been updated"}), 702
+
+def success703():
+    jsonify({"success": "The record has been deleted"}), 703
